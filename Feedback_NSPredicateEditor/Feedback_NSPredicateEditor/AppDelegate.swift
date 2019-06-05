57af3601def04f44b46f1c4c560329863128217a
Peter Kamb <peterkamb@gmail.com>
2019-06-05 10:33:12 -0700

    add new Cocoa application

---
+++ Feedback_NSPredicateEditor/Feedback_NSPredicateEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Feedback_NSPredicateEditor
+//
+//  Created by Feedback on 6/5/19.
+//  Copyright © 2019 Feedback. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
