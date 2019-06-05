65212e125857e4f4ccbbe0326419c670c6b811cd
Peter Kamb <peterkamb@gmail.com>
2019-06-05 12:06:24 -0700

    add interesting templates to reproduce bugs

--- Feedback_NSPredicateEditor/Feedback_NSPredicateEditor/ViewController.swift
+++ Feedback_NSPredicateEditor/Feedback_NSPredicateEditor/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        predicateEditor.rowTemplates = NSPredicateEditorRowTemplate.feedbackTemplates()
+        
         predicateEditor.addRow(self)
     }
 
@@ -30,3 +32,40 @@ class ViewController: NSViewController {
 
 }
 
+extension NSPredicateEditorRowTemplate {
+    
+    static func feedbackTemplates() -> [NSPredicateEditorRowTemplate] {
+        
+        let compound = NSPredicateEditorRowTemplate(compoundTypes: [NSCompoundPredicate.LogicalType.and, .not, .or].map({ $0.rawValue as NSNumber }))
+        
+        let row1 = NSPredicateEditorRowTemplate(leftExpressions: [NSExpression(forKeyPath: "freeform")],
+                                                  rightExpressionAttributeType: .stringAttributeType,
+                                                  modifier: .direct,
+                                                  operators: [NSComparisonPredicate.Operator.equalTo.rawValue as NSNumber],
+                                                  options: 0)
+        
+        let row2 = NSPredicateEditorRowTemplate(leftExpressions: [NSExpression(forKeyPath: "single select")],
+                                                 rightExpressions: [NSExpression(forKeyPath: "option 1")],
+                                                 modifier: .direct,
+                                                 operators: [NSComparisonPredicate.Operator.equalTo.rawValue as NSNumber],
+                                                 options: 0)
+        
+        let row3 = NSPredicateEditorRowTemplate(leftExpressions: [NSExpression(forKeyPath: "multi select")],
+                                                rightExpressions: [NSExpression(forKeyPath: "option 1"),
+                                                                   NSExpression(forKeyPath: "option 2"),
+                                                                   NSExpression(forKeyPath: "option 3")],
+                                                modifier: .direct,
+                                                operators: [NSComparisonPredicate.Operator.equalTo.rawValue as NSNumber,
+                                                            NSComparisonPredicate.Operator.lessThan.rawValue as NSNumber,
+                                                            NSComparisonPredicate.Operator.greaterThan.rawValue as NSNumber],
+                                                options: 0)
+        
+        return [
+            compound,
+            row1,
+            row2,
+            row3,
+        ]
+    }
+    
+}
